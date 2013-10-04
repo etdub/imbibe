bf53fb3a6337fe34d23289ce01110dbae06788ce
Eric Wong <eric.wong.t@gmail.com>
2013-10-04 00:50:25 +0000

    Move to 4 spaces. Incoming msg can be many metrics.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='imbibe',
-    version='0.0.2',
+    version='0.0.3',
     description='Client to stream metrics from multiple Speakeasy servers',
     author='Eric Wong',
     py_modules=['imbibe'],
